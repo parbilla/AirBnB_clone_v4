75187cc3024f8b3f8d5bf5b7b3acc9f6fbfcd1cb
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-15 13:52:00 -0300

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,18 @@
-$('document').ready(function () {
-    new_list = {};
-    $('INPUT[type="checkbox"]').change(function(){
-	let id = $(this).attr('data-id');
-	let name = $(this).attr('data-name');
+$(document).ready(function () {
+    let new_dict = {};
+    $('INPUT[type=checkbox]').change(function (){
+	let id = $(this).data('id');
+	let name = $(this).data('name');
 	if ($(this).is(':checked')){
-	    new_list[id] = name;
+	    new_dict[id] = name;
 	}
-	else {
-	    delete new_list[id];
+	if (!$(this).is(':checked')) {
+	    delete new_dict[id];
 	}
-
+	const new_list = []
+	for (const name in new_dict){
+	    new_list.push(new_dict[name]);
+	}
+	$('div.amenities h4').text(new_list.join(', '));
     });
-    
 });
