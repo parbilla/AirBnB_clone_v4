fe580c7b31be27e67fd2c7d430f9818ace4807a8
parbilla <parbilla@gmail.com>
2021-05-16 16:04:49 +0000

    task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    let new_dict = {};
+    $('INPUT[type=checkbox]').change(function() {
+        let id = $(this).data('id');
+        let name = $(this).data('name');
+        if ($(this).is(':checked')) {
+            new_dict[id] = name;
+        }
+        if (!$(this).is(':checked')) {
+            delete new_dict[id];
+        }
+        const new_list = []
+        for (const name in new_dict) {
+            new_list.push(new_dict[name]);
+        }
+        $('div.amenities h4').text(new_list.join(', '));
+    });
+});
+$.getJSON('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+    } else {
+        $('#api_status').removeClass('available');
+    }
+});
