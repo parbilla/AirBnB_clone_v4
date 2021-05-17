621d977bce0e90a8e841f541965ef62618700817
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-17 19:12:20 -0300

    put Request inside de onload

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -15,11 +15,11 @@ $(document).ready(function() {
         }
         $('div.amenities h4').text(new_list.join(', '));
     });
-});
-$.getJSON('http://0.0.0.0:5001/api/v1/status/', function(data) {
-    if (data.status === 'OK') {
-        $('#api_status').addClass('available');
-    } else {
-        $('#api_status').removeClass('available');
-    }
+    $.getJSON('http://0.0.0.0:5001/api/v1/status/', function(data) {
+	if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+	} else {
+            $('#api_status').removeClass('available');
+	}
+    });
 });
